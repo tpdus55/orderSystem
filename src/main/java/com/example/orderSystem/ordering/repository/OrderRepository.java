1969eabcafaeda63b77e26c09db68b944cf9b278
tpdus55 <tpdustkfkd55@gmail.com>
2026-02-04 16:28:29 +0900

    내 주문목록 조회 -> member의 이메일을 찾아서 맞으면 목록조회 가능하게 설정

--- src/main/java/com/example/orderSystem/ordering/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.orderSystem.ordering.repository;
-
-import com.example.orderSystem.ordering.domain.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order,Long> {
-}
